d9b96bad65eea46e5fef558b10412566d6be2636
JTMax1 <johntochukwumax@gmail.com>
2026-02-08 11:32:09 +0100

    fix: correct date formatting in subscription result view

--- resources/views/user/premium/subscription-result.blade.php
+++ resources/views/user/premium/subscription-result.blade.php
@@ -17,8 +17,14 @@
 				</thead>
 				<tbody>
 					<tr>
-						<td>{{$subscription->created_at->format('D dS, M Y')}}</td>
-						<td>{{$subscription->expiry->format('D dS, M Y')}}</td>
+					    <!--Format was wrong. the format() only works on Carbon/DateTime objects, NOT strings. causes a crash - updated by JTMax 20012026-->
+						<!--<td>{{$subscription->created_at->format('D dS, M Y')}}</td>-->
+						<!--<td>{{$subscription->expiry->format('D dS, M Y')}}</td>-->
+						
+						<!--The following works better. Parse the date in Blade (FAST FIX) ✔ Works even if DB stores strings ✔ No controller changes needed-->
+						<td>{{ \Carbon\Carbon::parse($subscription->created_at)->format('D dS, M Y') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($subscription->expiry)->format('D dS, M Y') }}</td>
+
 						<td>₦{{$subscription->amount}}</td>
 						<td>₦{{$subscription->vat}}</td>
 						<th>₦{{$subscription->amount + $subscription->vat}}</th>
